9b77715f56b4604725b76bc1ddb931b9d95dff2f
satrop <stevenportas@me.com>
2025-02-15 07:51:17 -0800

    Set up Astro project with GitHub Actions for deployment

--- src/Components/navigation/NavigationLink.jsx
+++
@@ -1,9 +0,0 @@
-import { forwardRef } from 'react';
-
-const NavigationLink = forwardRef(({ href, text }, ref) => (
-	<a ref={ref} href={href} className="nav-link">
-		<span className="sr-only">{text}</span>
-	</a>
-));
-
-export default NavigationLink;
