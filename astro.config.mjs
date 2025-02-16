106733b1c9cdd20ab845ca9a9ef2be1bc0529499
Steven Portas <stevenportas@me.com>
2025-02-16 15:37:55 -0800

    Manually updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/',
   integrations: [
     icon({
       include: {
