73c5ea52d0beb06be925d0343f9aa0fc511a4448
Steven Portas <stevenportas@me.com>
2025-02-17 06:38:55 -0800

    Cleaning up my Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-portfolio/',
-  plugins: [react()],
-  css: {
-    devSourcemap: true,
-  },
+
+	plugins: [react()],
+	css: {
+		devSourcemap: true,
+	},
 });
